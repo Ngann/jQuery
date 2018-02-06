56368679fba336999f7992f40b4103c9a29f797f
Ngann <ngancamnguyen@gmail.com>
2018-02-05 19:53:37 -0800

    add bootstrap styles sheet

---
+++ js/peekscripts.js
@@ -0,0 +1,11 @@
+
+$(document).ready(function() {
+  $(".clickable").click(function() {
+    $("#walrus-showing").toggle();
+    $("#walrus-hidden").toggle();
+  });
+
+  $(".fadein").fadeIn(function(){
+    $("#fadein")
+  })
+});
